53435750832c64c3902f20290ac648252ab4a6c4
murumart <lythr0naxe@gmail.com>
2024-11-08 16:36:01 +0200

    tiny control

--- scenes/rpg_battle/gregmovie/player_char_selection.gd
+++ scenes/rpg_battle/gregmovie/player_char_selection.gd
@@ -1,1 +1,21 @@
 extends Control
+
+@onready var tabs: TabContainer = $Tabs
+
+
+func _ready() -> void:
+	$Tabs/Main/MainMenu/InfoButton.pressed.connect(func():
+		tabs.current_tab = 2
+	)
+	$Tabs/Main/MainMenu/ActButton.pressed.connect(func():
+		tabs.current_tab = 3
+	)
+	$Tabs/Main/MainMenu/MoveButton.pressed.connect(func():
+		tabs.current_tab = 1
+	)
+
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			tabs.current_tab = 0
